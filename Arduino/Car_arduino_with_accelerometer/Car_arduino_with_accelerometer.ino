b806a5db924e481b36ee0b8368c6f9da57d1f92c
vochta <robotlabjournal1@gmail.com>
2017-04-23 00:18:05 +0300

    to work2

--- Arduino/Car_arduino_with_accelerometer/Car_arduino_with_accelerometer.ino
+++ Arduino/Car_arduino_with_accelerometer/Car_arduino_with_accelerometer.ino
@@ -91,13 +91,15 @@ byte turn_PWM = 70;
 
 //*************************** Variables begin *********************************
 
+int i; // simple counter
 
 Adafruit_SSD1306 display(OLED_MOSI, OLED_CLK, OLED_DC, OLED_RESET, OLED_CS);
 
 uint8_t buf[VW_MAX_MESSAGE_LEN];
 uint8_t buflen = VW_MAX_MESSAGE_LEN;
-String buf_string;
-int command_hesh; 
+int command_message = 5; 
+bool new_message_available = false;
+
 int current_command;
 bool training_mode = false;
 
@@ -219,12 +221,12 @@ void turn_left ()
 {
     int i;
     buf_string = "";
-    command_hesh = 0; 
+    command_message = 0; 
    
     for (i = 0; i < buflen; i++)
     {
       Serial.println("markG1");  
-      command_hesh += buf[i];
+      command_message += buf[i];
       buf_string += char(buf[i]);
       Serial.println("markG2");
     }
@@ -368,6 +370,8 @@ void loop()
 {
     t_loop_start = millis();
     digitalWrite(LED, LOW);
+    
+//******************  Begin get message from PC (high-level)  *****************
     buf[VW_MAX_MESSAGE_LEN];
     buflen = VW_MAX_MESSAGE_LEN;
     if (vw_get_message(buf, &buflen)) // Non-blocking
@@ -375,19 +379,12 @@ void loop()
         digitalWrite(12, true); // Flash a light to show received good message
         // Message with a good checksum received, dump it.
 
-    //        GetMessage();
-    //*******************************
-
-        int i;
-        buf_string = "";
-        command_hesh = 0; 
-       
+        // get message
+        command_message = 0; 
         for (i = 0; i < buflen; i++)
         {
-          command_hesh += buf[i];
-          buf_string += char(buf[i]);
+          command_message += buf[i];
         }
-<<<<<<< HEAD
     
         new_message_available = true;
         digitalWrite(12, false);
@@ -397,12 +394,8 @@ void loop()
         new_message_available = false;
     }
 //******************  End get message from PC (high-level)  *****************
-=======
->>>>>>> parent of 917efe5... Work1
 
-    //*******************************
 
-<<<<<<< HEAD
     display.setCursor(0,0);
     display.clearDisplay();
     display.println(micros());
@@ -416,22 +409,10 @@ void loop()
     if (new_message_available == true)
     {    
         switch (command_message)  // what to do? (special commands)
-=======
-        display.setCursor(0,0);
-        display.clearDisplay();
-    //    display.println(micros());
-    //    display.println(buf_string);
-        display.println(command_hesh);
-        display.println(drive_PWM);
-        display.println(turn_PWM);
-        display.display();
-
-        switch (command_hesh)
->>>>>>> parent of 917efe5... Work1
         {
             case (5):      // 5
                 stop_car();
-                current_command = command_hesh;
+                current_command = command_message;
                 start_measurement = false;
                 command_movement_direction = 0;
             break;
@@ -457,7 +438,6 @@ void loop()
             break;      
             
             default:
-<<<<<<< HEAD
                 if (command_message != current_command)
                 {
                     t_command_execution_start = millis();
@@ -467,82 +447,6 @@ void loop()
                 }
         }    
     }
-=======
-                if (millis() > t_go_pause)  //can i go ?
-                {                     
-                    if ((command_hesh == current_command)||(training_mode)) // if the new command is equal to current command, keep doing it 
-                                                                            // if it is training mode now - just do the new command
-                    {
-                        switch (command_hesh)
-                        {
-                            case (47):      // /
-                                afterburner();
-                                delay(20);
-                            break;
-
-                            case (8):         // 8
-                                forward();
-                            break;
-
-                            case (2):         // 2
-                                backward();
-                            break;
-
-                            case (4):         // 4 
-                                turn_left();
-                            break;
-
-                            case (6):       //  6
-                                turn_right();
-                            break;      
-                        }
-                    }
-                    else //
-                    {
-                        switch (command_hesh)
-                        {
-                            case (47):      // / -simbol
-                                afterburner(); 
-                                delay(20);
-                            break;
-
-                            case (8):         // 8
-                                forward();
-                                command_movement_direction = 1;
-                            break;
-
-                            case (2):         // 2
-                                backward();
-                                command_movement_direction = -1;
-                            break;
-
-                            case (4):         // 4 
-                                turn_left();
-                            break;
-
-                            case (6):       //  6
-                                turn_right();
-                            break;      
-                        }
-                    
-                        t_command_execution_start = millis();
-                        v_start = Vy_current;
-                        
-                        start_measurement = true;
-                        current_command = command_hesh;      
-                    //   stop_car();
-                    //    delay(200);
-                    }
-                }
-        }   
-        
-        digitalWrite(12, false);
-        
-        Serial.println("*******************");
-    } 
-  
-    
->>>>>>> parent of 917efe5... Work1
     
     if (millis() > t_go_pause)  //can i go ?
     {                     
@@ -658,9 +562,8 @@ void loop()
         }
         else if (abs(v_delta) > v_target_max)
         {
-         //   stop();
-         //   t_go_pause = millise() + go_pause_lenght;
-            stop_car();
+         //   t_go_pause = millis() + go_pause_lenght;
+         //   stop_car();
             display.println("Too fast");
             display.print("Vdelta = ");
             display.println(v_delta);                
